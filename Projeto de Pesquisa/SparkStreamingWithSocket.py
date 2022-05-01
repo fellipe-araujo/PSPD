88219b94b667dbd2338c65b89369a13f8fd73448
Fellipe Araujo <araujo.fellipe@hotmail.com>
2022-05-01 12:26:41 -0300

    change from spark context to spark sql

--- Projeto de Pesquisa/SparkStreamingWithSocket.py
+++ Projeto de Pesquisa/SparkStreamingWithSocket.py
@@ -1,98 +1,60 @@
-import os
-import shutil
 import sys
-
-from pyspark import SparkContext
-from pyspark.streaming import StreamingContext
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import explode
+from pyspark.sql.functions import split
 from tabulate import tabulate
+from datetime import datetime
 
-# Obter ou registrar a variável brodcast
-def getWordExcludeList(sparkContext):
-	if ('wordExcludeList' not in globals()):
-		globals()['wordExcludeList'] = sparkContext.broadcast(["a", "b", "c"])
+initial_time = datetime.now().strftime('%d/%m/%y %H:%M:%S')
 
-	return globals()['wordExcludeList']
+def generate_metrics(df, epoch_id):
+  data = df.collect()
+  dataTable = []
+  broken = '\n=========================================\n'
+  columnNames = ["Palavras", "Quantidade"]
 
-# Obter ou registrar um acumulador
-def getDroppedWordsCounter(sparkContext):
-	if ('droppedWordsCounter' not in globals()):
-		globals()['droppedWordsCounter'] = sparkContext.accumulator(0)
+  if data:
+    now = datetime.now().strftime('%d/%m/%y %H:%M:%S')
+    print(broken)
+    counts = "TOTAL DE DADOS RECEBIDOS ATÉ "
+    print(counts + now)
 
-	return globals()['droppedWordsCounter']
+    total_words = 0
 
-def createContext(host, port, outputPath):
-	# Se o print abaixo não for impresso, isso significa que o StreamingContext
-	# foi carregado do novo ponto de verificação
-	print("Creating new context")
+    for item in data:
+      dataTable.append([item[0], item[1]])
+      total_words += item[1]
 
-	if os.path.exists(outputPath):
-		os.remove(outputPath)
+    print(tabulate(dataTable, headers=columnNames, tablefmt="fancy_grid"))
+    
+    print("\nQUANTIDADE TOTAL DE PALAVRAS RECEBIDAS: {}".format(total_words))
+    print(broken)
 
-	sc = SparkContext(appName="PythonStreamingRecoverableNetworkWordCount")
-	sc.setLogLevel("WARN")
-	ssc = StreamingContext(sc, 1)
+    period = 'INTERVALO DE TEMPO MONITORADO: {}  -  {}'.format(initial_time, now)
+    total_words_result = '\nQUANTIDADE TOTAL DE PALAVRAS RECEBIDAS: {}'.format(total_words)
 
-	# Cria um fluxo de socket stream para o host e port de destino e
-	# conta as palavras no fluxo de entrada a partir do delimitador
-	lines = ssc.socketTextStream(host, port)
-	words = lines.flatMap(lambda line: line.split(" "))
-	wordCounts = words.map(lambda x: (x, 1)).reduceByKey(lambda x, y: x + y)
+    with open("./logs",'w') as f:
+      pass
 
-	def echo(time, rdd):
-		# Obter ou registrar uma lista de exclusão -> broadcast
-		excludeList = getWordExcludeList(rdd.context)
-
-		# Obter ou registrar um acumulador de contador de palavras soltas
-		droppedWordsCounter = getDroppedWordsCounter(rdd.context)
-
-		# Usa o "excludeList" para descartar palavras repetidas e
-		# usa o "droppedWordsCounter" para contá-las
-		def filterFunc(wordCount):
-			if wordCount[0] in excludeList.value:
-				droppedWordsCounter.add(wordCount[1])
-				return False
-			else:
-				return True
-
-		data = rdd.filter(filterFunc).collect()
-		qtd_words = 0
-		dataTable = []
-		broken = '\n=========================================\n'
-		columnNames = ["Palavras", "Quantidade"]
-
-		if len(data) > 0:
-			print(broken)
-			counts = "DADOS RECEBIDOS EM %s" % (time)
-			print(counts)
-
-			for item in data:
-				qtd_words += item[1]
-				dataTable.append([item[0], item[1]])
-
-			print(tabulate(dataTable, headers=columnNames, tablefmt="fancy_grid"))
-			
-			print("\nQUANTIDADE DE PALAVRAS RECEBIDAS: {}".format(qtd_words))
-			print("LOG SALVO EM " + os.path.abspath(outputPath))
-			print(broken)
-
-			header_out = counts + "\nQUANTIDADE DE PALAVRAS RECEBIDAS: {}".format(qtd_words)
-			
-			with open(outputPath, 'a') as f:
-				f.write(header_out + "\n\n" + "%s" % (tabulate(dataTable, headers=columnNames, tablefmt="fancy_grid")) + '\n' + broken + '\n')
-
-	wordCounts.foreachRDD(echo)
-	return ssc
+    with open('./logs', 'a') as f:
+      f.write(period + "\n" + total_words_result + "\n\n" + "%s" % (tabulate(dataTable, headers=columnNames, tablefmt="fancy_grid")) + '\n' + broken + '\n')
 
 if __name__ == "__main__":
-	if len(sys.argv) != 5:
-		print("Usage: recoverable_network_wordcount.py <hostname> <port> <checkpoint-directory> <output-file>", file=sys.stderr)
-		sys.exit(-1)
+	if len(sys.argv) != 3:
+		print('Usage: SparkStreamingWithSocket.py <hostname> <port>')
 
-	host, port, checkpoint, output = sys.argv[1:]
+	spark = SparkSession.builder.appName("StructuredNetworkWordCount").getOrCreate()
 
-	if os.path.exists("checkpoint"):
-		shutil.rmtree("checkpoint")
+  # Create DataFrame representing the stream of input lines from connection to localhost:9999
+	lines = spark.readStream.format("socket").option("host", sys.argv[1]).option("port", int(sys.argv[2])).load()
 
-	ssc = StreamingContext.getOrCreate(checkpoint, lambda: createContext(host, int(port), output))
-	ssc.start()
-	ssc.awaitTermination()
+  # Split the lines into words
+	words = lines.select(explode(split(lines.value, " ")).alias("word"))
+
+  # Generate running word count
+	wordCounts = words.groupBy("word").count()
+
+  # Start running the query that prints the running counts to the console
+	query = wordCounts.writeStream.foreachBatch(generate_metrics).outputMode("update").start()
+
+	query.awaitTermination()
